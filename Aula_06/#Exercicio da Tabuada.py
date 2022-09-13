4d76f8f3bb2fd0ed72eeefbd3017828f8fb531ce
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2022-09-13 20:53:13 -0300

    Add files via upload

---
+++ Aula_06/#Exercicio da Tabuada.py
@@ -0,0 +1,8 @@
+#Exercicio da Tabuada
+
+for num in range(1,11):
+    print(f"Tabuada do {num}")
+    
+    for num2 in range(1,11):
+        resultado = num * num2
+        print(f'{num} x {num2} = {resultado}')
