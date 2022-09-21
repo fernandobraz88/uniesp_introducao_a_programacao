2945ca3a718a1d22bb533a785152c25739a37508
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2022-09-20 23:03:55 -0300

    Commit

---
+++ Aula_06/Exercicio 07.py
@@ -0,0 +1,8 @@
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+email = input("Digite o seu e-mail: ")
+
+print(f'Nome:{nome}')
+print(f'Idade:{idade}')
+print(f'e-mail:{email}')
+print('Parabens!Dados cadastrados com sucesso!')
