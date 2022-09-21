90b251db583e3cb71fb485bf7bbaf6fe4c030226
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2022-09-20 22:25:22 -0300

    Commit

---
+++ Aula_06/Exercicio 06.py
@@ -0,0 +1,11 @@
+convidados = ['Chaves','Quico','Chiquinha','Nhonho','Popis'] 
+
+for nome in convidados:
+    print(f"Querido(a) {nome}, venha a minha casa hoje comer sanduiche de presunto e tomar refresco de procedencia duvidosa")  
+
+print("Popis não poderá ir ao jantar") 
+
+convidados_2 = ['Chaves','Quico','Chiquinha','Nhonho'] 
+
+for nome in convidados_2:
+    print(f"Querido(a){nome}, venha a minha casa hoje comer sanduiche de presunto e tomar refresco de procedencia duvidosa")
