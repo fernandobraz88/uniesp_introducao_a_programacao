3f2d18c783c64897925e29b7b3c8cdaae8809d06
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2022-09-22 07:06:15 -0300

    Mudança do nome da pasta

---
+++ Aula_03_+Exercicios/Aula03_Exercicio2.py
@@ -0,0 +1,4 @@
+#Crie um programa em Python que pergunte o nome do usuário e imprima um bom dia com o nome do usuário. Dica: você pode utilizar o método .format() ou uma concatenação de string, por exemplo.
+
+nome = input('Digite se nome: ')
+print(f'(Bom dia {nome}!!!)')
---
+++ Aula_03_+Exercicios/Aula03_Exercicio4.py
@@ -0,0 +1,4 @@
+#Crie um programa em Python que imprima um convite para uma festa com o nome do usuário que for digitado. O nome do usuário precisará ser apresentado todo em caixa alta.
+
+Nome=input('Digite o nome do convidado: ').upper()
+print(f'(Olá {Nome}, você está convidado para encher a cara no meu casório!!!)')
