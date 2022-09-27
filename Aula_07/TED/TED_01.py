e8e4dac977c4eb3be8fa3b3d7c0e62da456874af
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2022-09-26 23:43:04 -0300

    Exercicio 1 completo

---
+++ Aula_07/TED/TED_01.py
@@ -0,0 +1,13 @@
+#As maçãs custam R$ 1,30 cada se forem compradas menos de uma dúzia,
+# e R$ 1,00 se forem compradas pelo menos 12.
+#  Escreva um programa que leia o número de maçãs compradas, 
+# calcule e escreva o custo total da compra.
+
+macas=float(input('Quantas maçãs deseja comprar?: '))
+
+if macas < 12:
+    custo=macas*1.30
+    print(f'Para {macas} maçãs, você irá gastar R${custo}')
+else:
+    custo=macas*1
+    print(f'Para {macas} maçãs, você irá gastar R${custo}')
