1783ebb5c5e25463421188cabf6563a6da236c1b
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2022-09-30 23:34:07 -0300

    Exercicio6_ok

--- Aula_08/Exercicio_06.py
+++ Aula_08/Exercicio_06.py
@@ -1,6 +1,14 @@
-import math
+from math import factorial
 
-num=int(input('Digite um numero: '))
-print(f'{num}! = {math.factorial(num)}')
+n=int(input('Informe um número inteiro para calcular o seu fatorial: '))
 
+f=factorial(n)
 
+v=n
+
+while v > 0:
+    print(f'{v}', end ='')
+    print(' x ' if v>1 else ' = ', end ='')
+    v-=1
+print(f'{f}')
+#print(f'O fatorial de {n} é {f}.')
