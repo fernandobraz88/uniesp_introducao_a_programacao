5806353be41b6776aab06b2216e2b3e727c8e7de
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2022-09-30 21:30:28 -0300

    Atualização_do_exerc_05

--- Aula_08/Exercicio_05.py
+++ Aula_08/Exercicio_05.py
@@ -9,23 +9,25 @@ while numeros >=0:
 
     numeros=float(input('Digite numeros aleatorios (positivos e negativos):'))
     
-    match numeros:
+    if numeros >=0 and numeros <=25:
+        lista1.append(numeros)
         
-        case numeros >=0 and numeros <=25:
-                lista1.append(numeros)
+    elif numeros >=26 and numeros <=50:
+        lista2.append(numeros)
         
-        case numeros >=26 and numeros <=50:
-                lista2.append(numeros)
+    elif numeros >=51 and numeros <=75:
+        lista3.append(numeros)
         
-        case numeros >=51 and numeros <=75:
-                lista3.append(numeros)
-        
-        case numeros >=76 and numeros <=100:
-                lista4.append(numeros)
+    elif numeros >=76 and numeros <=100:
+        lista4.append(numeros)
 
-        case numeros: < 0:
-                print('a quantidade de numeros entre:')
-                print(f'0-25 = {len(lista1)}')
-                print(f'26-50 = {len(lista2)}')
-                print(f'51-75 = {len(lista3)}')
-                print(f'76-100 = {len(lista4)}')
+    elif numeros > 100:
+        numeros
+
+    else:
+        print('a quantidade de numeros entre:')
+        print(f'0-25 = {len(lista1)}')
+        print(f'26-50 = {len(lista2)}')
+        print(f'51-75 = {len(lista3)}')
+        print(f'76-100 = {len(lista4)}')
+
--- Aula_08/Teste.py
+++ Aula_08/Teste.py
@@ -1,7 +1,4 @@
-from email.policy import default
-
-#testando exercicio 05 com if/elif/else
-
+#por quê o exercicio não funcionou com match/case ????
 
 lista1=[]
 lista2=[]
@@ -12,26 +9,25 @@ numeros=1000
 
 while numeros >=0:
 
-    numeros=float(input('Digite numeros aleatorios (positivos e negativos):'))
+    numeros=int(input('Digite numeros aleatorios (positivos e negativos):'))
     
-    if numeros >=0 and numeros <=25:
-        lista1.append(numeros)
+    match numeros:
         
-    elif numeros >=26 and numeros <=50:
-        lista2.append(numeros)
+        case numeros >=0 and numeros <=25:
+                lista1.append(numeros)
         
-    elif numeros >=51 and numeros <=75:
-        lista3.append(numeros)
+        case numeros >=26 and numeros <=50:
+                lista2.append(numeros)
         
-    elif numeros >=76 and numeros <=100:
-        lista4.append(numeros)
+        case numeros >=51 and numeros <=75:
+                lista3.append(numeros)
+        
+        case numeros >=76 and numeros <=100:
+                lista4.append(numeros)
 
-    elif numeros > 100:
-        numeros
-
-    else:
-        print('a quantidade de numeros entre:')
-        print(f'0-25 = {len(lista1)}')
-        print(f'26-50 = {len(lista2)}')
-        print(f'51-75 = {len(lista3)}')
-        print(f'76-100 = {len(lista4)}')
+        case numeros: < 0:
+                print('a quantidade de numeros entre:')
+                print(f'0-25 = {len(lista1)}')
+                print(f'26-50 = {len(lista2)}')
+                print(f'51-75 = {len(lista3)}')
+                print(f'76-100 = {len(lista4)}')
