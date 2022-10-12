d578218d20afe5ab22bd996fd3c7f48532108c29
fernandobraz88 <fernandobraz88@gmail.com>
2022-10-11 21:12:54 -0300

    atualização

---
+++ Testes/Exemplo_tupla_dicionario.py
@@ -0,0 +1,22 @@
+tupla_teste=('endpoint', 200)
+
+print(tupla_teste[0])
+print(tupla_teste[1])
+
+#tupla nao admite modificações. 
+
+dicionário={
+        1:{
+            'nome':'João'
+            'idade': 33
+            'email':'joão@gmail.com'
+            'telefone': 88991010
+                }
+        2:{
+            'nome':'Pedro'
+            'idade':45
+            'email':'pedro@gmail.com'
+            'telefone':99005582
+
+                }
+            }
