14bed2fd62fd69c513e35963f369386513a0877e
fernandobraz88 <fernandobraz88@gmail.com>
2022-10-11 21:17:12 -0300

    atualização

--- Testes/Exemplo_tupla_dicionario.py
+++ Testes/Exemplo_tupla_dicionario.py
@@ -5,18 +5,20 @@ print(tupla_teste[1])
 
 #tupla nao admite modificações. 
 
-dicionário={
+dicionario={
         1:{
-            'nome':'João'
-            'idade': 33
-            'email':'joão@gmail.com'
+            'nome':'João',
+            'idade': 33,
+            'email':'joão@gmail.com',
             'telefone': 88991010
-                }
+                },
         2:{
-            'nome':'Pedro'
-            'idade':45
-            'email':'pedro@gmail.com'
+            'nome':'Pedro',
+            'idade':45,
+            'email':'pedro@gmail.com',
             'telefone':99005582
 
-                }
+                },
             }
+
+print(dicionario[1])
