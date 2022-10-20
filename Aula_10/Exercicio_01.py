4c7edf2a578d845f494de1c6ce860c952f50721f
Fernando Braz da Silveira <112355109+fernandobraz88@users.noreply.github.com>
2022-10-20 12:52:04 -0300

    Ainda inconcluso

--- Aula_10/Exercicio_01.py
+++ Aula_10/Exercicio_01.py
@@ -4,7 +4,26 @@
 # Depois escrever a mensagem ACHEI, se o nome estiver entre os 10 nomes lidos anteriormente
 # ou NÃO ACHEI caso contrário.
 
-lista_times=[]
+times=[]
 
-while lista_times in range(0,9):
-    lista_times=input("Insira o nome de um Time de futebol:")
+while len(times)<10:
+    time=input("Insira o nome de um Time de futebol:").upper()
+    
+    if time in times:
+        print('Você ja inseriu esse Time de Futebol')
+    else:
+        times.append(time)
+
+print(times)
+
+consulta=input('Qual o time que você deseja procurar na lista?').upper()
+ordem=times.index(consulta)
+
+for consulta in times:
+    if consulta in times==True:
+        print(f'Achei, {consulta} está na lista, na posição {ordem}')
+
+    else:
+        print('Não Achei')
+
+    break
