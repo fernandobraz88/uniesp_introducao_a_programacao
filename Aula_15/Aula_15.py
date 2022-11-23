ebdd4f1759039a99a94d1b1317aac7cc330bf775
fernandobraz88 <fernandobraz88@gmail.com>
2022-11-23 19:19:52 -0300

    update

---
+++ Aula_15/Aula_15.py
@@ -0,0 +1,21 @@
+#-------------------Cod de Leitura--------------------------------
+
+#with open ('Aula15.txt','r', encoding='utf-8') as arquivo:
+#    conteudo = arquivo.read()
+#    print(conteudo)
+
+#with open('Aula15.txt','r',encoding='utf-8') as arquivo:
+#    x=1
+#    for linha in arquivo:
+#        print(f'{x} contendo {linha}')
+#    x=x+1
+
+#--------------------Cod de Escrita ------------------------------
+#nome=Messias"
+#with open ("arquivo_da_aula.txt", "w", encoding="utf-8") as aquivo:
+#    for line in range (10):
+#        arquivo.write(f'{line}- Meu nome é {nome}\n')
+
+#"W" sobrescreve o texto anterior
+
+#"A" usando o A no lugar do W o texto é adicionado
